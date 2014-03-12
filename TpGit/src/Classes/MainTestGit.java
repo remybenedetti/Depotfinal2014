81be78bc6e940d321cbe8335117bbcffb53fa584
Remy <remybenedetti@remybenedetti@hotmail.fr>
2014-03-12 10:44:20 +0100

    premier commit

---
+++ TpGit/src/Classes/MainTestGit.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class MainTestGit {
+	 public static void main (String[] args)
+	    {
+	        
+	    	// Modif Annie 10/03/14
+	    	Atelier monAtelier;
+	    	monAtelier= new Atelier(1,1,"info",10);
+	    	System.out.print ("Voici mon premier atelier : ");
+	    	System.out.print (monAtelier.getLibelleatelier());
+	    	
+	    }
+
+}
