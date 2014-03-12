b57951332427663d471321f912b727cea8806c4a
Remy <remybenedetti@remybenedetti@hotmail.fr>
2014-03-12 10:57:39 +0100

    ajout classe Categorie

---
+++ TpGit/src/Classes/Categorie.java
@@ -0,0 +1,18 @@
+package Classes;
+
+public class Categorie {
+	
+	private Integer idcategorie;
+	private String libellecategorie;
+	// constructeur
+	public Categorie(Integer wid,String wlib) {
+		idcategorie = wid;
+		libellecategorie = wlib;
+	}
+	public Integer getIdcategorie() {
+		return idcategorie;
+	}
+	public String getLibellecategorie() {
+		return libellecategorie;
+	}
+}
